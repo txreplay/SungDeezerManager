43f7c2f2988b958b6f25804de253ddeb3f3340fe
Valentin Commenge <txreplay@gmail.com>
2016-05-26 02:51:41 +0200

    firebase working

--- src/AppBundle/Services/FirebaseService.php
+++ src/AppBundle/Services/FirebaseService.php
@@ -7,9 +7,9 @@ use GuzzleHttp\Client as GuzzleClient;
 
 class FirebaseService
 {
-    private $defaul_url = 'https://sung-2481f.firebaseio.com/';
-    private $default_token = 'AIzaSyDYLhMiUps9aup8qrehQcGAdpbhtAnfpZk';
-    private $default_path = '/';
+    private $defaul_url = 'https://sung.firebaseio.com/';
+    private $default_token = 'R3EaAojB9LbFJxGu8e1RDWcsWi0M5s8GjJT6yW9F';
+    private $default_path = '/data';
 
     function __construct()
     {
@@ -18,9 +18,6 @@ class FirebaseService
 
     public function pushSongToFirebase($playlistId, $song, $author='Valentin')
     {
-//        $this->firebase->push($this->default_path.'/'.$playlistId.'/'.$song, $author);
-        $value = $this->firebase->get($this->default_path);
-        var_dump($value);
-        die();
+        $this->firebase->push($this->default_path.'/'.$playlistId.'/'.$song, $author);
     }
 }
